d9c0a2887133c1d5e799aa988d2673f34a4fbbd7
rafy <jinhlee19@gmail.com>
2022-02-23 01:39:26 +0900

    pr-2

--- pr-1/src/App.js
+++ pr-1/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-500 h-30">
+      <h1 className="text-8xl text-blue-400 bg-slate-600">Hello World</h1>
     </div>
   );
 }
 
 export default App;
+
+// 요청된 tailwind css만 build되는 상태.
+// npm start 이전에 호출된 className이 아니면 실행되지 않는다.

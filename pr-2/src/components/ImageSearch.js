99a4269ede2651cb34cb6b20205c6da9118eb0b8
rafy <jinhlee19@gmail.com>
2022-02-24 16:24:27 +0900

    Step 5 - err

--- pr-2/src/components/ImageSearch.js
+++ pr-2/src/components/ImageSearch.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const ImageSearch = () => {
-  return (
-    <div>ImageSearch</div>
-  )
-}
+const ImageSearch = ({ searchText }) => {
+	const [text, setText] = useState('');
+	const onSubmit = (e) => {
+		searchText(text);
+		e.preventDefault();
+	};
+	return (
+		<div className="max-w-sm mx-auto my-10">
+			<form onSubmit={onSubmit} className="w-full max-w-sm">
+				<div className="flex items-center border-b  border-teal-500 py-2">
+					<input onChange={e => setText(e.target.value)} type="text" placeholder="Search Image Term..." className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" />
+					<button className="flex-shrink-0 bg-teal-500 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+						Search
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
 
-export default ImageSearch
+export default ImageSearch;

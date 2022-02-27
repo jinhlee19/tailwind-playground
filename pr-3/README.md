ac2751a01c92e449dc9cd46b9864a287db47feac
rafy <jinhlee19@gmail.com>
2022-02-27 23:16:17 +0900

    PR-3 Setup 1

---
+++ pr-3/README.md
@@ -0,0 +1,1 @@
+# Sass + Tailwind Setup
---
+++ pr-3/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['SUIT','ui-sans-serif', 'system-ui'],
+      'serif': ['SUIT','ui-serif', 'Georgia'],
+      'mono': ['SUIT','ui-monospace', 'SFMono-Regular'],
+      'display': ['Jalnan'],
+      'body': ['SUIT'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}

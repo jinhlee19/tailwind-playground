e0781f9f3772509c8cfba803f6326cce9fac5e9b
rafy <jinhlee19@gmail.com>
2022-02-27 23:58:29 +0900

    try 1 -err

--- pr-3/README.md
+++ pr-3/README.md
@@ -1,1 +1,25 @@
 # Sass + Tailwind Setup
+
+```json
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+```
+
+to
+
+```json
+"scripts": {
+    "start": "npm run build:css && react-scripts start",
+    "build": "npm run build:css && react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject",
+    "build:css": "postcss src/styles/tailwind.scss -o src/styles/index.scss "
+  }
+```
+
+참고: [https://adebola-niran.medium.com/how-to-setup-tailwindcss-with-create-react-app-and-s-b7243607c3e7]
+=> 하지만 실패... (2021.11.5 이후 v3. 찾아봐야할듯.)
--- pr-3/tailwind.config.js
+++ pr-3/tailwind.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    fontFamily: {
-      'sans': ['SUIT','ui-sans-serif', 'system-ui'],
-      'serif': ['SUIT','ui-serif', 'Georgia'],
-      'mono': ['SUIT','ui-monospace', 'SFMono-Regular'],
-      'display': ['Jalnan'],
-      'body': ['SUIT'],
-    },
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx,}', './public/index.html'],
+	theme: {
+		fontFamily: {
+			sans: ['SUIT', 'ui-sans-serif', 'system-ui'],
+			serif: ['SUIT', 'ui-serif', 'Georgia'],
+			mono: ['SUIT', 'ui-monospace', 'SFMono-Regular'],
+			display: ['Jalnan'],
+			body: ['SUIT'],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
